7687555c3977a7c8d586f900b7864fe3dcf21a5d
Daniel Rich <danr14@gmail.com>
2017-04-10 16:24:38 -0500

    Preparing for merge

--- src/server_code/UDP_Handshake.java
+++ src/server_code/UDP_Handshake.java
@@ -5,9 +5,15 @@ import java.net.*;
 import java.util.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-
+/*
+ * This class is the UDP_Handshake thread. 
+ * When an incoming packet arrives at the UDP socket, pull the packet. 
+ * Check the IP address the packet originated from, if it exists in currentConnections, grab the queue for this packet from currentConnections.
+ * If the IP address doesn't exist in currentConnections, create a new blockingQueue and map the IP address to the new blockingQueue
+ */
 public class UDP_Handshake extends Thread
 {
+
 
     //log on client send "hello" to server (UPD connection)
 	Map<InetAddress,BlockingQueue<DatagramPacket>> currentConnections = new HashMap<InetAddress,BlockingQueue<DatagramPacket>>();
@@ -16,13 +22,13 @@ public class UDP_Handshake extends Thread
 	
 	public UDP_Handshake(String name) throws IOException
 	{
-		socket = new DatagramSocket(4445);
+		socket = new DatagramSocket(4445); //Create a welcome socket on port 4445
 	}
 	
 	
 	public void run()
 	{
-		BlockingQueue<DatagramPacket> tempQueue = null;
+		BlockingQueue<DatagramPacket> tempQueue = null; 
 			
 		while(true)
 		{

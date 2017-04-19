bfa1c3de8fa18024a2d10cfb6df8c61ea28c82d5
Baoan Nguyen <baoansayz@gmail.com>
2017-04-18 20:14:54 -0500

    Client progress

--- src/client_code/client.java
+++ src/client_code/client.java
@@ -1,14 +1,39 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
 public class Client{
 
+	private DatagramSocket datagramSocket ;
+	private InetAddress address;
+	private DatagramPacket packet;
+	private byte[] buffer;
 	private String name;
 	private String clientID;
 	private int key;
 
-	Client (){
+	Client ()throws SocketException, UnknownHostException{
+		buffer = new byte[1024];
+		datagramSocket = new DatagramSocket();
+		address = InetAddress.getLocalHost();
+		packet = new DatagramPacket(buffer, buffer.length, address, 8888);
 	}
 
-	public void sendLogin(){
+	public void sendLogin(InetAddress address, String username, DatagramSocket datagramSocket)throws IOException{
 		// send "Hello" msg to SERVER
+		System.out.println("Sending HELLO");
+		byte[] buffer = username.getBytes();
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 4445);
+		// send packet
+		datagramSocket.send(packet);
 	}
 
 	public void chatRequest(String client){
@@ -21,8 +46,10 @@ public class Client{
 		// send message to
 	}
 
-	public void rcvMSG(Datagram msg){
-		// switch (msg)??
+	public void rcvMSG(DatagramPacket msg){
+		// gets msg
+		String rcv = unpack(msg);
+		// switch (rcv)??
 			//case 1: 
 
 			//case 2: 
@@ -42,6 +69,22 @@ public class Client{
 		// disconnect from server when user types "Log off"
 	}
 
+	public static String unpack(DatagramPacket p1)
+	{
+		// upaacks datagram into readable format
+		String str = new String(
+		p1.getData(),
+		p1.getOffset(),
+		p1.getLength(),
+		StandardCharsets.UTF_8 // or some other charset
+	);
+	return str;
+	}
+public static void main(String[] args) throws UnknownHostException, SocketException, IOException, NoSuchAlgorithmException  {
 
+	System.out.println("Hello, world");
+	Client a = new Client();
+
+}
 
 }
